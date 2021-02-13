fde85c166fa793fd2b2bd695e62c67c0d2794c6c
campillo <loritec26@gmail.com>
2021-02-12 20:30:56 -0500

    primer commit

--- src/main/java/com/loricode/controllers/ControllerProduct.java
+++ src/main/java/com/loricode/controllers/ControllerProduct.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +30,18 @@ public class ControllerProduct {
 	public List<Product> getProducts() {
 		return repository.findAll();
 	}
+	
+	@PostMapping
+	public Object addProduct(@RequestBody Product product) {
+		return repository.save(product);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public void deleteProduct(@PathVariable int id) {
+		repository.deleteById(id);
+	}
+	
+	
+		
 	
 }
